fca721c28ea8db21fa03fc73c214ae896f1c3d66
mrkenz9595 <79914545+mrkenz9595@users.noreply.github.com>
2023-05-04 10:21:00 +0700

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,4 +14,4 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 svn co https://github.com/RADIUSdesk/openwrt-meshdesk/trunk/files files
 svn co https://github.com/RADIUSdesk/openwrt-meshdesk/trunk/luci-app-meshdesk feeds/luci/applications/luci-app-meshdesk
-svn co https://github.com/RADIUSdesk/openwrt-meshdesk/trunk/MESHdesk /package/MESHdesk
+svn co https://github.com/RADIUSdesk/openwrt-meshdesk/trunk/MESHdesk package/MESHdesk
