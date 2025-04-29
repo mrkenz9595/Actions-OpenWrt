586ca656999d73ea05924ca95d0f006c84836cd1
mrkenz9595 <79914545+mrkenz9595@users.noreply.github.com>
2025-04-29 19:09:47 +0700

    Update custom-config.sh

--- custom-config.sh
+++ custom-config.sh
@@ -11,9 +11,9 @@
 #echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_bananapi_bpi-r3-mini=y' >> $BUILD_ROOT/.config
 #echo 'CONFIG_TARGET_ROOTFS_PARTSIZE=4096' >> $BUILD_ROOT/.config
 
-echo 'CONFIG_TARGET_mediatek=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_mediatek_filogic=y' >> $BUILD_ROOT/.config
-echo 'CONFIG_TARGET_mediatek_filogic_DEVICE_abt_asr3000=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_TARGET_ramips=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_TARGET_ramips_mt7621=y' >> $BUILD_ROOT/.config
+echo 'CONFIG_TARGET_ramips_mt7621_DEVICE_xiaomi_mi-router-3g=y' >> $BUILD_ROOT/.config
 
 # Users & Groups Support
 #echo 'CONFIG_PACKAGE_shadow-utils=y' >> $BUILD_ROOT/.config
